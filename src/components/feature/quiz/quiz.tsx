59539c1544c962adea3bf573e2d05c3ba81f7415
AlfaGruisPL <alfagruis@gmail.com>
2024-05-22 15:10:11 +0200

    init

--- src/components/feature/quiz/quiz.tsx
+++ src/components/feature/quiz/quiz.tsx
@@ -1,10 +1,21 @@
 import style from './quiz.module.scss'
 import {questions} from "../../../data/questions.ts";
-function Quiz() {
+import QuizTopBar from "./quizTopBar/quizTopBar.tsx";
+interface QuizInterface{
+    questionNumber:number;
+    questionCount:number
+}
+function Quiz(props:QuizInterface) {
+
 const data = questions;
     return (
         <div className={style.quizContainer}>
-            123
+            <QuizTopBar questionCount={data.length} questionNumber={1}/>
+            <div>
+
+                tu pytania będą
+
+            </div>
             </div>
     )
 }
